e6eee14a9d8b47daa92b9c445361e53501aae0ef
Francisco  Muñoz <francisco@BerserkMac.local>
2023-04-14 12:40:33 -0400

    codigos

--- python/matriz.py
+++ python/matriz.py
@@ -1,31 +1,38 @@
-def solicitar_datos_persona(i, nombres, ruts):
-    nombre = input("Ingrese nombre de la persona {}: ".format(i + 1))
-    rut = input("Ingrese RUT de la persona {}: ".format(i + 1))
-    nombres[i] = nombre
-    ruts[i] = rut
 
-    if i < 3:
-        solicitar_datos_persona(i + 1, nombres, ruts)
+filas = int(input("Ingrese el numero de filas a usar:"))
+columnas = int(input("Ingrese el numero de filas a usar:"))
+
+matriz = [[0 for j in range(columnas)] for i in range(filas)]
 
 
-def mostrar_listas_ordenadas(nombres, ruts):
-    print("\nLista de nombres antes de ordenarla: ", nombres)
-    print("Lista de RUTs antes de ordenarla: ", ruts)
-
-    # Ordena alfabeticamente
-    nombres.sort()
-    print("Lista de nombres ordenada: ", nombres)
-
-    # Ordena ascendente y alfabeticamente
-    ruts.sort()
-    print("Lista de RUTs ordenada: ", ruts)
+for i in range(filas):
+    for j in range(columnas):
+        valor = input(f"Ingrese el valor para la fila {i+1}, columna {j+1}: ")
+        matriz[i][j] = valor
 
 
-nombres = [""]
-ruts = [""] 
+print("Matriz ingresada:")
+for fila in matriz:
+    print(fila)
 
-# Solicita datos de las personas
-solicitar_datos_persona(0, nombres, ruts)
+# Alfabeticamente
+for fila in matriz:
+    fila.sort()
 
-# Mostrar listas ordenadas
-mostrar_listas_ordenadas(nombres, ruts)
+
+print("Matriz ordenada por filas:")
+for fila in matriz:
+    print(fila)
+
+# numéricamente 
+for i in range(columnas):
+    columna = [matriz[j][i] for j in range(filas)]
+    columna.sort()
+    for j in range(filas):
+        matriz[j][i] = columna[j]
+
+
+print("Matriz ordenada por columnas:")
+for fila in matriz:
+    print(fila)
+
