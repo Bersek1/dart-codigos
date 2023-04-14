43a256e16d4101eef618d854cf980d27c8f6a085
Francisco  Muñoz <francisco@BerserkMac.local>
2023-04-14 14:11:16 -0400

    codigos

--- 03-funcionesbasicas.dart
+++ 03-funcionesbasicas.dart
@@ -1,8 +1,12 @@
 main() {
   var op1 = operacion(20, 10, suma);
   var op2 = operacion(20, 10, resta);
+  var op3 = operacion(20, 10, mul);
+  var op4 = operacion(20, 10, div);
   print(op1);
   print(op2);
+  print(op3);
+  print(op4);
 }
 
 int operacion(int a, int b, Function func) {
@@ -15,4 +19,12 @@ int suma(int a, int b) {
 
 int resta(int a, int b) {
   return a - b;
+}
+
+int mul(int a, int b) {
+  return a * b;
+}
+
+int div(int a, int b) {
+  return a ~/ b;
 }
