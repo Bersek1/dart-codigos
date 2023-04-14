009a500c17bcaf37887e2ab8d081d4e024b9a563
Francisco  Muñoz <francisco@BerserkMac.local>
2023-04-14 14:14:24 -0400

    codigos

--- 04-funcionabreviada.dart
+++ 04-funcionabreviada.dart
@@ -1,10 +1,16 @@
 main() {
-  var op1 = operacion(10, 20, suma);
-  var op2 = operacion(10, 20, resta);
+  var op1 = operacion(20, 10, suma);
+  var op2 = operacion(20, 10, resta);
+  var op3 = operacion(20, 10, mul);
+  var op4 = operacion(20, 10, div);
   print(op1);
   print(op2);
+  print(op3);
+  print(op4);
 }
 
 int operacion(int a, int b, Function func) => func(a, b);
 int suma(int a, int b) => a + b;
 int resta(int a, int b) => a - b;
+int mul(int a, int b) => a * b;
+int div(int a, int b) => a ~/ b;
