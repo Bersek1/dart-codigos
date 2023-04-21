09144d0493d50fb12a6ab2345f74a4ff8d9713aa
Francisco  Muñoz <francisco@BerserkMac.local>
2023-04-21 12:40:30 -0400

    codigos

--- laboratorio/multiplicarmatriz.py
+++ laboratorio/multiplicarmatriz.py
@@ -2,13 +2,13 @@ import random
 
 filas = int(input("ingrese la cantidad de filas: "))
 columnas = int(input("ingrese la cantidad de columnas: "))
-escalar = int(input("ingrese el valor a multiplicar: "))
+multiplicar = int(input("ingrese el valor a multiplicar: "))
 
 matriz = [[random.randint(0, 10) for x in range(columnas)] for y in range(filas)]
 
 for x in range(filas):
     for y in range(columnas):
-        matriz[x][y] *= escalar
+        matriz[x][y] *= multiplicar
 
 print("la matriz multiplicada:")
 for fila in matriz:
